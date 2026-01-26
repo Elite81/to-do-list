12f5089cbf23db023da4fe734f0c8eb3c2fa73e4
Elite81 <schophiled@outlook.com>
2026-01-26 21:36:18 +0000

    Add domain to the ALOWED_HOST

--- ToDoList/settings.py
+++ ToDoList/settings.py
@@ -27,11 +27,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = os.environ.get("DJANGO_DEBUG", "False") == "True"
-DEBUG = True
+DEBUG = os.environ.get("DJANGO_DEBUG", "False") == "True"
+# DEBUG = True
 
 
-ALLOWED_HOSTS = ["todlist.onrender.com", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["tasks-7ugz.onrender.com", "localhost", "127.0.0.1"]
 
 
 
